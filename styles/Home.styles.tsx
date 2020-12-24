53d9317463af20146747f87eef4328ca781b0b62
Terence Jeong <terence.jeong@gmail.com>
2020-12-24 14:43:37 +1100

    [FEATURE] - added styled-components

---
+++ styles/Home.styles.tsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  min-height: 100vh;
+  padding: 0 0.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const Main = styled.main`
+  padding: 5rem 0;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 50px;
+`;
+
+const Footer = styled.footer`
+  width: 100%;
+  height: 100px;
+  border-top: 1px solid #eaeaea;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const HomeStyles = {
+  Container,
+  Title,
+  Main,
+  Footer,
+};
